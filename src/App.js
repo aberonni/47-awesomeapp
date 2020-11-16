fbf3a13b0c788f4d284bcef233a5d83d5b44024a
Domenico Gemoli <domenicogemoli@gmail.com>
2020-11-16 17:05:01 +0100

    Add Footer component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import "./App.css";
 
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -172,22 +173,7 @@ function App() {
         <div className="line "></div>
       </div>
 
-      <div className="section-footer">
-        <img
-          className="logo"
-          src="https://learntocodetogether.nl/assets/logo.svg"
-        />
-        <div className="title">Created by</div>
-        <div className="subtitle">
-          Rein Op &#x27;t land &amp; Danny van der Jagt
-        </div>
-        <div className="team-container">
-          <img
-            className="team"
-            src="https://learntocodetogether.nl/assets/danny&rein.svg"
-          />
-        </div>
-      </div>
+      <Footer />
     </div>
   );
 }
